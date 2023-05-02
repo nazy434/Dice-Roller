56d5de26ad38709be4d784ddb0993b274ac252c9
Naz K. Abadi <nazanin.k.abadi@gmail.com>
2023-05-02 16:04:41 -0400

    Added a toast button to show button clicks

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var toast: Toast? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            toast?.cancel()
+            toast = Toast.makeText(this, "Hey", Toast.LENGTH_SHORT)
+            toast?.show()
+        }
     }
 }
