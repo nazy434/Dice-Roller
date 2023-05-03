86dcf9c3ff3dd65f7b0024c634dadcaa90ed402b
Naz K. Abadi <nazanin.k.abadi@gmail.com>
2023-05-03 17:11:38 -0400

    Implemented Dice roller, using VectorDrawable Support library

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,19 +3,34 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.ImageView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
-    private var toast: Toast? = null
+    private lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
-            toast?.cancel()
-            toast = Toast.makeText(this, "Hey", Toast.LENGTH_SHORT)
-            toast?.show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        diceImage.setImageResource(
+            when (randomInt) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                6 -> R.drawable.dice_6
+                else -> R.drawable.empty_dice
+            }
+        )
     }
 }
